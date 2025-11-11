aa2d209ea3c378b30819abaeda9bb7b14475226c
Julien Baouz <baouzjulien@gmail.com>
2025-11-11 16:04:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Tableau des thèmes
+const themes = [
+  {name: 'Light', message: 'Hello sunshine — Light theme is on!'},
+  {name: 'Dark', message: 'The night is yours — Dark theme is on!'},
+  {name: 'Ocean', message: 'Blue skies and high tides — Ocean theme is on!'},
+  {name: 'Nord', message: 'The frost has settled - Nord theme is on!'}
+];
 
+// Définition des variables
+const menu = document.getElementById('theme-dropdown');
+const button = document.querySelector('button');
+
+// Affichage du menu
+button.addEventListener('click', () => {
+  const isHidden = menu.toggleAttribute('hidden');
+  button.setAttribute('aria-expanded', String(!isHidden));
+});
